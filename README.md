542164f43513e55bf78fbd3317455e56ac8e8748
zhangzhong <im.zhong@outlook.com>
2023-02-22 22:01:59 +0800

    add conan fmt

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # zcc
 Zhong's C Compiler
 
+# create
+`conan new cmake_exe -d name=xxx -d version=xxx`
+
+# install and build dependencies
+`conan install .`
+
 # build
-`conan build --profile=debug|release .`
+`conan build [--profile=debug|release] .`
+
+# custom command
+1. conan clean
+2. conan fmt 调用clang-format给所有的代码排版 `conan fmt .`
---
+++ conan/cmd_fmt.py
@@ -0,0 +1,38 @@
+from conan.api.conan_api import ConanAPI
+from conan.api.output import ConanOutput, Color
+from conan.cli.command import OnceArgument, conan_command
+from conans.client.userio import UserInput
+import os
+import subprocess
+
+
+@conan_command(group="Custom commands")
+def fmt(conan_api: ConanAPI, parser, *args):
+    """
+    Use clang-format to fmt current directory
+    """
+
+    if not os.path.exists("conanfile.py"):
+        ConanOutput().error("conanfile.py not found")
+        return
+
+    if not os.path.exists(".clang-format"):
+        ConanOutput().error(".clang-format not found")
+        return
+
+    # 执行clang-format命令
+    # 这还不如让python遍历一下当前目录呢
+    # 这个牛逼了 os.walk会帮你递归的遍历文件 方便
+    for root, _, files in os.walk("src"):
+        for file in files:
+            if file.endswith((".h", ".hpp", ".c", ".cxx", ".cpp")):
+                filename = os.path.join(root, file)
+                print(filename)
+                subprocess.run(["clang-format", "-i", filename])
+
+    for root, _, files in os.walk("test"):
+        for file in files:
+            if file.endswith((".h", ".hpp", ".c", ".cxx", ".cpp")):
+                filename = os.path.join(root, file)
+                print(filename)
+                subprocess.run(["clang-format", "-i", filename])
