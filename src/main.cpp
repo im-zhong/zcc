40b48f633f0e5179eae6bfb4a15b734169fddbf1
zhangzhong <im.zhong@outlook.com>
2023-02-23 11:02:05 +0800

    delete conan generated files

--- src/main.cpp
+++
@@ -1,3 +0,0 @@
-#include "zcc.h"
-
-int main() { zcc(); }
--- src/zcc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef _WIN32
-#define ZCC_EXPORT __declspec(dllexport)
-#else
-#define ZCC_EXPORT
-#endif
-
-ZCC_EXPORT void zcc();
